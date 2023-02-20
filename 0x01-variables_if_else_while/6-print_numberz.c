9a675c67055225d87180db9f68104413dfd3bf86
Mahmoud Haney <mahmoudhaneysaeed@gmail.com>
2023-02-20 13:31:06 +0200

    Betty Style

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
@@ -10,9 +8,9 @@
 int main(void)
 {
 	int a = 0;
-	
+
 	for (; a <= 9 ; ++a)
-		putchar('0' + a); // To make it 'char'
+		putchar('0' + a);
 	putchar('\n');
 	return (0);
 }
