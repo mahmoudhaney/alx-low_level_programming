85b52290519f22b355d8918088a094a863695d06
Mahmoud Haney <mahmoudhaneysaeed@gmail.com>
2023-02-22 06:29:22 +0200

    Initial task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/**
+ * positive_or_negative - prints if n i is positive or negative
+ * @i: inteher to use
+ * Return: 1 or 0
+ */
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+#endif
