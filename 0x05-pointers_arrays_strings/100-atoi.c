78be258932a8c42e99a911e0ce865dd9d51f4f8e
Mahmoud Haney <mahmoudhaneysaeed@gmail.com>
2023-02-27 11:42:07 +0200

    All Tasks

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,45 @@
 #include "main.h"
-#include "2-strlen.c"
-
 /**
- * _atoi - converts string to integer
- * @s: string to convert
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: returns integer value
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
+	int i, d, n, len, f, digit;
 
-	for (i = 0; i < _strlen(s); i++)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
 		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
+			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			c++;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
+		i++;
 	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	} else
-	{
-		num *= -1;
-	}
-	return (num);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
